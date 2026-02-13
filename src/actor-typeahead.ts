1c9d66b6de38bcf976de471faa6a86f85df86c25
Tijs Teulings <hello@tijs.org>
2026-02-13 22:44:18 +0100

    Add modified work copyright to source header

--- src/actor-typeahead.ts
+++ src/actor-typeahead.ts
@@ -2,9 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *
- * Copyright (c) 2026 Jake Lazaroff
+ * Original work Copyright (c) 2026 Jake Lazaroff
+ * Modified work Copyright (c) 2026 Tijs Teulings
  *
- * Repository: https://tangled.org/jakelazaroff.com/actor-typeahead
+ * Based on: https://tangled.org/jakelazaroff.com/actor-typeahead
  */
 
 const template = document.createElement("template");
